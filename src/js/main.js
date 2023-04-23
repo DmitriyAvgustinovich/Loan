a8ca9e05bed76e4f7c281818b852b96f24be7006
Dmitry <dmitryavgn@mail.ru>
2023-04-23 14:17:41 +0500

    реализовал просмотр ютуб-видео в модальном окне

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import { Slider } from './modules/index'
+import { VideoPlayer } from './modules/index'
 
 window.addEventListener('DOMContentLoaded', () => {
     const slider = new Slider('.page', '.next')
     slider.render()
+
+    const player = new VideoPlayer('.showup .play', '.overlay')
+    player.init()
 })
